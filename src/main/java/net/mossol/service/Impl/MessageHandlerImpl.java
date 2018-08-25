5f98a4be8916e803382ea19c031b28aea152fa39
amos.doan <amos.doan@gmail.com>
2018-08-25 17:59:48 +0900

    Arrange code     - Remove HttpConnection Class     - Remove Autowired annotation (Replaced by Resource annotation)

--- src/main/java/net/mossol/service/Impl/MessageHandlerImpl.java
+++ src/main/java/net/mossol/service/Impl/MessageHandlerImpl.java
@@ -2,18 +2,14 @@ package net.mossol.service.Impl;
 
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.linecorp.centraldogma.client.Watcher;
 import net.mossol.connection.RetrofitConnection;
 import net.mossol.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import net.mossol.HttpConnection;
 import net.mossol.MossolUtil;
 import net.mossol.service.MenuServiceHandler;
 import net.mossol.service.MenuServiceHandler.FoodType;
@@ -23,10 +19,6 @@ import net.mossol.util.MessageBuildUtil;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
-import static net.mossol.model.SimpleText.SimpleTextType.SHOW_MENU_D;
-import static net.mossol.model.SimpleText.SimpleTextType.SHOW_MENU_J;
-import static net.mossol.model.SimpleText.SimpleTextType.SHOW_MENU_K;
-
 /**
  * Created by Amos.Doan.Mac on 2017. 12. 6..
  */
@@ -35,7 +27,6 @@ public class MessageHandlerImpl implements MessageHandler {
     private static final Logger logger = LoggerFactory.getLogger(MessageHandlerImpl.class);
     private static final String REPLY_URI = "https://api.line.me/v2/bot/message/reply";
     private static final String LEAVE_URI = "https://api.line.me/v2/bot/group/%s/leave";
-    private static final HttpConnection httpConnection = new HttpConnection();
     private volatile Map<String, SimpleText> simpleTextContext;
     private volatile List<RegexText> regexTextContext;
 
@@ -74,19 +65,17 @@ public class MessageHandlerImpl implements MessageHandler {
 
     private boolean sendFoodRequest(String token, FoodType foodType) {
         MenuInfo menu = menuServiceHandler.selectMenu(foodType);
-        return sendRequest(REPLY_URI, MessageBuildUtil.sendFoodMessage(token, menu));
+        return sendRequest(MessageBuildUtil.sendFoodMessage(token, menu));
     }
 
-    private boolean sendRequest(String uri, LineReplyRequest request) {
+    private boolean sendRequest(LineReplyRequest request) {
         String payload = MossolUtil.writeJsonString(request);
         logger.debug("sendRequest Payload : {}", payload);
-        //return httpConnection.post(uri, payload);
         retrofitConnection.sendReply(request);
         return true;
     }
 
     private boolean leaveRoom(String groupId) {
-        //return httpConnection.post(uri, payload);
         retrofitConnection.leaveRoom(null, groupId);
         return true;
     }
@@ -100,24 +89,24 @@ public class MessageHandlerImpl implements MessageHandler {
                 switch (regex.getType()) {
                     case ADD_MENU_K:
                         addMenuResult = menuServiceHandler.addMenu(result, FoodType.KOREA_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, addMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, addMenuResult));
                     case ADD_MENU_J:
                         addMenuResult = menuServiceHandler.addMenu(result, FoodType.JAPAN_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, addMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, addMenuResult));
                     case ADD_MENU_D:
                         addMenuResult = menuServiceHandler.addMenu(result, FoodType.DRINK_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, addMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, addMenuResult));
                     case DEL_MENU_K:
                         String removeMenuResult = menuServiceHandler.removeMenu(result, FoodType.KOREA_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, removeMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, removeMenuResult));
                     case DEL_MENU_J:
                         removeMenuResult = menuServiceHandler.removeMenu(result, FoodType.JAPAN_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, removeMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, removeMenuResult));
                     case DEL_MENU_D:
                         removeMenuResult = menuServiceHandler.removeMenu(result, FoodType.DRINK_FOOD);
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, removeMenuResult));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, removeMenuResult));
                     case TEXT:
-                        return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, regex.getResponse()));
+                        return sendRequest(MessageBuildUtil.sendTextMessage(token, regex.getResponse()));
                 }
             }
         }
@@ -129,16 +118,15 @@ public class MessageHandlerImpl implements MessageHandler {
         switch (simpleText.getType()) {
             case SHOW_MENU_K:
                 menuCandidate = menuServiceHandler.getMenu(FoodType.KOREA_FOOD);
-                return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, menuCandidate));
+                return sendRequest(MessageBuildUtil.sendTextMessage(token, menuCandidate));
             case SHOW_MENU_J:
                 menuCandidate = menuServiceHandler.getMenu(FoodType.JAPAN_FOOD);
-                return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, menuCandidate));
+                return sendRequest(MessageBuildUtil.sendTextMessage(token, menuCandidate));
             case SHOW_MENU_D:
                 menuCandidate = menuServiceHandler.getMenu(FoodType.DRINK_FOOD);
-                return sendRequest(REPLY_URI, MessageBuildUtil.sendTextMessage(token, menuCandidate));
+                return sendRequest(MessageBuildUtil.sendTextMessage(token, menuCandidate));
             case TEXT:
-                return sendRequest(REPLY_URI,
-                        MessageBuildUtil.sendTextMessage(token, simpleText.getResponse()));
+                return sendRequest(MessageBuildUtil.sendTextMessage(token, simpleText.getResponse()));
             case SELECT_MENU_K:
                 return sendFoodRequest(token, FoodType.KOREA_FOOD);
             case SELECT_MENU_J:
@@ -147,8 +135,6 @@ public class MessageHandlerImpl implements MessageHandler {
                 return sendFoodRequest(token, FoodType.DRINK_FOOD);
             case LEAVE_ROOM:
                 String groupId =  event.getSource().getGroupId();
-                String uri = String.format(LEAVE_URI, groupId);
-                //return sendRequest(uri, null);
                 return leaveRoom(groupId);
         }
         return false;
