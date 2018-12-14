e5c68c59dcc66b2eedcbaca20a289ba8ae57633b
amos.doan <amos.doan@gmail.com>
2018-12-14 22:48:23 +0900

    Add MossolMessageController

--- src/main/java/net/mossol/context/AnnotatedServiceConfiguration.java
+++ src/main/java/net/mossol/context/AnnotatedServiceConfiguration.java
@@ -2,6 +2,7 @@ package net.mossol.context;
 
 import javax.annotation.Resource;
 
+import net.mossol.MossolMessageController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,11 +17,21 @@ public class AnnotatedServiceConfiguration {
     @Resource
     MossolLineController mossolLineController;
 
+    @Resource
+    MossolMessageController mossolMessageController;
+
     @Bean
     public AnnotatedServiceRegistrationBean mossolHandler() {
         return new AnnotatedServiceRegistrationBean().setServiceName("MOSSOL")
                                                      .setPathPrefix("/")
                                                      .setService(mossolLineController);
+    }
+
+    @Bean
+    public AnnotatedServiceRegistrationBean mossolMessageHandler() {
+        return new AnnotatedServiceRegistrationBean().setServiceName("MOSSOL_MSG")
+                                                     .setPathPrefix("/")
+                                                     .setService(mossolMessageController);
     }
 
     @Bean
